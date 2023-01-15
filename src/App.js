af4f627258c7cb71ebc3c4040a7c96e35d157e6d
Arpit kumar <rk9003278@gmail.com>
2023-01-15 18:50:55 +0530

    Adding Header, Footer and No page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './page/Home.js'
 import Nopage from './page/Nopage.js'
 import Singleproduct from './page/Singleproduct.js';
+import Header from './component/Header.js';
+import Footer from './component/Footer.js';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path='/singleproduct/:id' element={<Singleproduct />} />
           <Route path="*" element={<Nopage />} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   )
---
+++ src/component/Footer.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { AiFillFacebook, AiFillInstagram, AiFillTwitterCircle } from 'react-icons/ai'
+import { BsTelephoneFill } from 'react-icons/bs'
+import { MdEmail } from 'react-icons/md'
+import { HiLink } from 'react-icons/hi';
+import { BiCopyright } from 'react-icons/bi'
+import { SiMinutemailer } from 'react-icons/si'
+
+const Footer = () => {
+    return (
+        <div className='footercontent'>
+            <div className='footer'>
+                <div className='info footerdiv'>
+                    <div className='heading'>
+                        <h5>Fakeshop</h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis eveniet, nisi</p>
+                </div>
+                <div className='suscribe footerdiv'>
+                    <div className='heading'>
+                        <h5>Suscribe Us Now</h5>
+                    </div>
+                    <div className='inputs'>
+                        <input type="text" name='update' placeholder='Enter email' />
+                        <SiMinutemailer className="icon" />
+                    </div>
+                </div>
+                <div className='social footerdiv'>
+                    <div className='heading'>
+                        <h5>Follow Us On</h5>
+                    </div>
+                    <div className='socialicons'>
+                        <AiFillFacebook className='icon' />
+                        <AiFillInstagram className='icon' />
+                        <AiFillTwitterCircle className='icon' />
+                    </div>
+                </div>
+                <div className="contact footerdiv">
+                    <div className='heading'>
+                        <h5>Call Us</h5>
+                    </div>
+                    <h5><BsTelephoneFill /> 9393939393</h5>
+                    <h5><MdEmail /> rk9003278@gmail.com</h5>
+                    <h5><HiLink /> ArpitTech.netlify.app</h5>
+                </div>
+            </div>
+            <div className="copyright">
+                <h1>ArpitTech <BiCopyright /> All Right Reserved</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
