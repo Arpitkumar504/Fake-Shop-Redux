b705db88ff38d906d59c963d32492d4039d13f9e
Arpit kumar <rk9003278@gmail.com>
2023-01-27 14:57:13 +0530

    Adding Toastify in cart page

--- src/component/Cartcard.js
+++ src/component/Cartcard.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import FormatPrice from './Formatprice'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { AiFillMinusCircle, AiFillPlusCircle, AiFillDelete } from 'react-icons/ai'
 import { useDispatch } from 'react-redux'
 import { decrease, increase, remove } from '../redux/action/productaction'
 
 const Cartcard = ({ data: { id, price, image }, quantity }) => {
     const dispatch = useDispatch();
+    const showtoast = () => {
+        toast.success('Removed From Cart', {
+            position: "top-center",
+            theme: "light",
+            autoClose: 2000,
+        });
+    }
     return (
         <div className='cartcard'>
             <Link to={`/singleproduct/${id}`}>
@@ -21,14 +30,15 @@ const Cartcard = ({ data: { id, price, image }, quantity }) => {
                 <div className="quantitybox">
                     <AiFillPlusCircle className="icon" onClick={() => dispatch(increase(id))} />
                     <h5>{quantity}</h5>
-                    <AiFillMinusCircle className="icon" onClick={() => dispatch(decrease(id))} />
+                    <AiFillMinusCircle className="icon" onClick={() => { dispatch(decrease(id)) }} />
                 </div>
             </div>
             <div className="subtotal">
                 <h5>{<FormatPrice price={price * quantity} />}</h5>
             </div>
             <div className="remove">
-                <AiFillDelete className="icon" onClick={() => dispatch(remove(id))} />
+                <AiFillDelete className="icon" onClick={() => { dispatch(remove(id)); showtoast() }} />
+                <ToastContainer />
             </div>
         </div>
     )
